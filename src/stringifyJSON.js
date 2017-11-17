564871233082847e028c3c95841422b9456998e1
archana <archana.nayak@gmail.com>
2017-11-17 10:07:23 -0800

    "Add functionality for stringifyJSON to match JSON.stringify"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,40 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(obj === null){
+  	return 'null';
+  }
+  if(typeof obj === 'function'){
+  	return null;
+  }
+  if(typeof obj === 'undefined'){
+    return  null;
+  }
+  if(typeof obj === 'boolean'){
+    return '' + obj;
+  }
+  if(typeof obj === 'string'){
+    return  '"' + obj + '"';
+  }
+  if(typeof obj === 'number'){
+    return '' + obj;
+  }
+  if(Array.isArray(obj)){
+    var arrayString = '[';
+    var valueString = obj.map(function(element){
+        return '' + stringifyJSON(element);
+    });
+    return arrayString + valueString + ']';
+  }
+   if(typeof obj === 'object'){
+    var objKeys = Object.keys(obj);
+    var  propertyString = [];
+    objKeys.forEach(function(key){
+      var stringifiedValue = stringifyJSON(obj[key]);
+        if(stringifiedValue !== null){
+          propertyString.push( '"' + key + '":' + stringifiedValue  + '');
+        }
+    });
+    return '{' + propertyString.join(',') + '}';
+  } 	
 };
